7605fd837a723fddb7775780c876e583e17ffc10
sam DING <dingsai@caihong360.com>
2015-10-26 20:12:04 +0800

    增加选择日期，选择时间

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -56,8 +56,61 @@ angular.module('starter.controllers', [])
         
     	},
   	};
+    $scope.timePickerObject = {
+      inputEpochTime: (8 * 60 * 60),  //Optional
+      step: 15,  //Optional
+      format: 24,  //Optional
+      titleLabel: '选择开工时间',  //Optional
+      setLabel: '选择',  //Optional
+      closeLabel: '关闭',  //Optional
+      setButtonType: 'button-balanced',  //Optional
+      closeButtonType: 'button-stable',  //Optional
+      callback: function (val) {    //Mandatory
+        timePickerCallback(val);
+      }
+    };
 
+    function timePickerCallback(val) {
+      if (typeof (val) === 'undefined') {
+        console.log('Time not selected');
+      } else {
+        var selectedTime = new Date(val * 1000);
+        if (selectedTime.getUTCMinutes() === 0){
+            $scope.startTime = selectedTime.getUTCHours() + ':' + selectedTime.getUTCMinutes()+'0'
+        }else{
+            $scope.startTime = selectedTime.getUTCHours() + ':' + selectedTime.getUTCMinutes()
+        }
+      }
+    };
 
+    $scope.timePickerObject_end = {
+      inputEpochTime: (18 * 60 * 60),  //Optional
+      step: 15,  //Optional
+      format: 24,  //Optional
+      titleLabel: '选择完工时间',  //Optional
+      setLabel: '选择',  //Optional
+      closeLabel: '关闭',  //Optional
+      setButtonType: 'button-balanced',  //Optional
+      closeButtonType: 'button-stable',  //Optional
+      callback: function (val) {    //Mandatory
+        timePickerCallback_end(val);
+      }
+    };
+
+    function timePickerCallback_end(val) {
+      if (typeof (val) === 'undefined') {
+        console.log('Time not selected');
+      } else {
+        var selectedTime = new Date(val * 1000);
+        console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), ':', selectedTime.getUTCMinutes(), 'in UTC');
+        if (selectedTime.getUTCMinutes() === 0){
+            $scope.endTime = selectedTime.getUTCHours() + ':' + selectedTime.getUTCMinutes()+'0'
+        }else{
+            $scope.endTime = selectedTime.getUTCHours() + ':' + selectedTime.getUTCMinutes()
+        }
+        
+      }
+    };
 })
 
 .controller('AboutCtrl',function($scope){
